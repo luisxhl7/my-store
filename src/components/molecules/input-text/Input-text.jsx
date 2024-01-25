2b9a33c7d134b050c476638132bc9abc81285c60
Luis carlos <luisxhl7@gmail.com>
2024-01-24 20:34:40 -0500

    adding border color in the inputs if have error

--- src/components/molecules/input-text/Input-text.jsx
+++ src/components/molecules/input-text/Input-text.jsx
@@ -2,23 +2,36 @@ import React from 'react'
 import './input-text.scss'
 
 export const InputText = ({ id, placeholder, description, onInputChange, value, errorAlert, regEx, type}) => {
-    const handleChange = (event) => {
-      onInputChange(event);
-    };
+  
+  const handleChange = (event) => {
+    onInputChange(event);
+  };
   
     return (
       <div className='content-input'>
         <label className='content-input__label' htmlFor={id}>
           {description}
         </label>
-        <input
-          type={type}
-          id={id}
-          name={id}
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-        />
+        {value.trim() !== '' && !regEx.test(value) ?
+          <input
+            type={type}
+            id={id}
+            name={id}
+            value={value}
+            onChange={handleChange}
+            placeholder={placeholder}
+            className='content-input --border-error'
+          />
+          :
+          <input
+            type={type}
+            id={id}
+            name={id}
+            value={value}
+            onChange={handleChange}
+            placeholder={placeholder}
+          />
+        }
         {value.trim() !== '' && !regEx.test(value) ?
           <span className={'content-input__alert-error'}>{errorAlert}</span>
           :
